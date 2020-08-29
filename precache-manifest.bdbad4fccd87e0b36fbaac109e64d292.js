7c6fa7af2c1c0f9a62f61aecd33b8553cb843891
Amit Dhawan <amit.dhawan@outlook.com>
2020-08-29 19:25:56 +0530

    Updates

---
+++ precache-manifest.bdbad4fccd87e0b36fbaac109e64d292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40661f9fff9f793059e624270f69c9ff",
+    "url": "/my-portfolio-app/index.html"
+  },
+  {
+    "revision": "15602ccbb04117e56289",
+    "url": "/my-portfolio-app/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "463695d8e1ee28bf482c",
+    "url": "/my-portfolio-app/static/js/2.223480c7.chunk.js"
+  },
+  {
+    "revision": "15602ccbb04117e56289",
+    "url": "/my-portfolio-app/static/js/main.ec4b70f1.chunk.js"
+  },
+  {
+    "revision": "cbbc5dbd6cd384a24549",
+    "url": "/my-portfolio-app/static/js/runtime~main.32d670a5.js"
+  }
+]);
